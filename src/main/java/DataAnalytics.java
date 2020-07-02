10c19d977f0733b898a8d0d7af45905b322de928
Davide Zollo <davidedezollo@gmail.com>
2020-07-02 12:31:56 +0200

    prova git

--- src/main/java/DataAnalytics.java
+++ src/main/java/DataAnalytics.java
@@ -4,5 +4,6 @@ public class DataAnalytics {
 	public static void main(String[] args) {
 		Analytics analytics = new Analytics(args[0], args[1], args[2], args[3], args[4], args[5]);
 		analytics.run();
+	
 	}
 }
