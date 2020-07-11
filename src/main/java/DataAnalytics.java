590b8ebf32ae1d9c55b92c7f48329ea9b161737c
luca <lucacarloni2@gmail.com>
2020-07-11 11:41:57 +0200

    Cambiato file task

--- src/main/java/DataAnalytics.java
+++ src/main/java/DataAnalytics.java
@@ -3,7 +3,6 @@ import org.apache.spark.SparkConf;
 public class DataAnalytics {
 
 	public static void main(String[] args) {
-
 		
 		Analytics analytics = new Analytics(args[0], args[1], args[2], args[3], args[4], args[5]);
 		analytics.run();
